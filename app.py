26606f8875e6c75e5a0c922b57293d4c57b49fbf
Youssef <ynakles@gmail.com>
2018-09-01 21:02:44 -0400

    import all

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import json, indicoio, sqlite3
 from newsapi import *
-from dateutil.parser import *
+from dateutil import parser
 
 app = Flask(__name__, static_url_path="/static")
 
