4bd78afc0fc20cc88d4e165ea66b49e580f9e21a
Youssef <ynakles@gmail.com>
2018-09-04 00:48:48 -0400

    v3

--- app.py
+++ app.py
@@ -5,121 +5,125 @@ from newsapi import *
 app = Flask(__name__, static_url_path="/static")
 
 indicoio.config.api_key = '43751098d41ba733826cc75dd3173717'
+newsapi = NewsApiClient(api_key='b71d9eefeca94a1487e7fc9bf9964af8')
 
 @app.route('/')
 def main():	
-	conn = sqlite3.connect(":memory:")
+	conn = sqlite3.connect("homepage.db")
 	cursor = conn.cursor()
-	keyword = str(request.args.get( "keyword" , None ))
-	newsapi = NewsApiClient(api_key='b71d9eefeca94a1487e7fc9bf9964af8')
-
+	keyword= 'Cryptocurrencies'
 	cursor.execute(""" 
 	CREATE TABLE IF NOT EXISTS main(
-		Keyword TEXT UNIQUE,
 		Title TEXT,
 		Description TEXT,
 		URL TEXT,
 		IMGLink TEXT,
-		Publish TEXT,
-		Score INTEGER, 
-		IndexLine INTEGER
+		Score INTEGER,
+		sqltime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 	); 
 	""")
 
-	data = newsapi.get_everything(
-                                      sources='crypto-coins-news',
-                                      language='en',
-                                      sort_by='publishedAt',
-                                      )
+	data = newsapi.get_everything(q=keyword,
+	                              language='en',
+	                              sources='techcrunch,crypto-coins-news,the-verge',
+								  sort_by='publishedAt',
+	                              )
 
 	load = data['articles']
 	resultslist = []
 	scorelist = []
 	index = 0
-	limit = 7
+	limit = 20
 
 	for index, post in zip(range(limit), load):
 		title = post['title']
-		description = post['description']
-		url = post['url']
-		imglink = post['urlToImage']
-		datetime = post['publishedAt']
-		scoredesc = indicoio.sentiment(description) * 1.5
-		calc = (scoredesc*100)
-		verg = calc / 2
-		score = ("%.0f" % verg)
-		scorelist.append(float(score))
-		nline = title, description, url, imglink, datetime, score, index
-		cursor.execute('INSERT INTO main (Title, Description, URL, IMGLink, Publish, Score, IndexLine) VALUES (?, ?, ?, ?, ?, ?, ?)', (nline))
-		conn.commit()
-		
-	sumlist = (sum(scorelist))
-	lenght_list = (len(scorelist))
-	before = (sumlist/lenght_list)
-	average = float("%.0f" % before) * 1.5
+		if len(cursor.execute("SELECT Title FROM main WHERE Title = ?", (title,)).fetchall())  > 0:
+			pass
+		else:
+			description = post['description']
+			url = post['url']
+			imglink = post['urlToImage']
+			datetime = post['publishedAt']
+			scoredesc = indicoio.sentiment(title) * 1.5
+			calc = (scoredesc*100)
+			verg = calc / 2
+			score = ("%.0f" % verg)
+			scorelist.append(float(score))
+			nline = title, description, url, imglink, score
+			cursor.execute('INSERT INTO main (Title, Description, URL, IMGLink, Score) VALUES (?, ?, ?, ?, ?)', (nline))
+			conn.commit()
 
-	for row in cursor.execute("SELECT Title, Description, URL, IMGLink, Score, Publish FROM main ORDER BY Score DESC"):
+	for row in cursor.execute("SELECT Title, Description, URL, IMGLink, Score, sqltime FROM main ORDER BY Score DESC, sqltime DESC LIMIT 15"):
 		resultslist.append(row)
 
-	return render_template('main.html', resultslist=resultslist, keyword=keyword, average=average)
+	return render_template('main.html', resultslist=resultslist, keyword=keyword)
 
 
-############
+###
 
 @app.route('/analysis', methods=['GET','POST'])
 def results():
+
 	keyword = str(request.args.get( "keyword" , None ))
-	newsapi = NewsApiClient(api_key='b71d9eefeca94a1487e7fc9bf9964af8')
-	conn = sqlite3.connect(":memory:")
+	conn = sqlite3.connect("analysis.db")
 	cursor = conn.cursor()
 	cursor.execute(""" 
 	CREATE TABLE IF NOT EXISTS main(
-		Keyword TEXT UNIQUE,
+		Keyword TEXT,
 		Title TEXT,
 		Description TEXT,
 		URL TEXT,
 		IMGLink TEXT,
-		Publish TEXT,
 		Score INTEGER,
-		IndexLine INTEGER
+		sqltime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 	); 
 	""")
-	data = newsapi.get_everything(q=keyword,
-                                      sources='crypto-coins-news',
-                                      language='en',
-                                      sort_by='relevancy',
-								)
-	load = data['articles']
+
 	resultslist = []
 	scorelist = []
-	index = 0
-	limit = 12
 
-	for index, post in zip(range(limit), load):
-		title = post['title']
-		description = post['description']
-		datasource = str(post['source'])
-		url = post['url']
-		imglink = post['urlToImage']
-		datetime = post['publishedAt']
-		scoredesc = indicoio.sentiment(description) * 1.5
-		calc = (scoredesc*100)
-		verg = calc / 2
-		score = ("%.0f" % verg)
-		scorelist.append(float(score))
-		nline = title, description, url, imglink, datetime, score
-		cursor.execute('INSERT INTO main ( Title, Description, URL, IMGLink, Publish, Score) VALUES (?, ?, ?, ?, ?, ?)', (nline))
-		conn.commit()
-		
-	sumlist = (sum(scorelist))
-	lenght_list = (len(scorelist))
-	before = (sumlist/lenght_list)
-	average = float("%.0f" % before) * 1.5
+	pagenum = 1
+	num_pages = 5
 
-	for row in cursor.execute("SELECT Title, Description, URL, IMGLink, Score, Publish FROM main ORDER BY Score DESC"):
+
+	for pagenum in range(2, num_pages + 1):
+		data = newsapi.get_everything(    sources='crypto-coin-news',
+		                                  language='en',
+		                                  sort_by='publishedAt',
+		                                  page=pagenum,
+		                                  q=keyword
+									 )
+		load = data['articles']
+
+		for post in load:
+			title = post['title']
+			description = post['description']
+			url = post['url']
+			imglink = post['urlToImage']
+			if keyword in title:
+				scoredesc = indicoio.sentiment(title)
+				calc = (scoredesc*100)
+				score = ("%.2f" % calc)
+				scorelist.append(float(score))
+				nline = keyword, title, description, url, imglink, score
+				cursor.execute('INSERT INTO main (Keyword, Title, Description, URL, IMGLink, Score)  VALUES (?, ?, ?, ?, ?, ?)', (nline))
+				conn.commit()
+			else:
+				pass
+				
+	for row in cursor.execute("SELECT Title, Description, URL, IMGLink, Score, sqltime FROM main ORDER BY Score DESC LIMIT 16"):
 		resultslist.append(row)
 
-	return render_template('analysis.html', resultslist=resultslist, keyword=keyword, average=average)
+
+	# for row in cursor.execute("SELECT Score FROM main ORDER BY sqltime DESC LIMIT 20"):
+	# 	scorelist.append(row[0]) 
+
+	# sumlist = (sum(scorelist))
+	# lenght_list = (len(scorelist))
+	# before = (sumlist/lenght_list)
+	# average = float("%.0f" % before)
+
+	return render_template('analysis.html', resultslist=resultslist, keyword=keyword)
 
 
 @app.route('/analytics')
--- test.py
+++ test.py
@@ -1,7 +1,34 @@
+import indicoio, sqlite3
+from newsapi import *
 import dateutil.parser
 
-datestring = '2018-08-31 14:05:19+00:00 '
+#indicoio.config.api_key = '43751098d41ba733826cc75dd3173717'
 
-datetime = dateutil.parser.parse(datestring)
 
-print (datetime.time())
+newsapi = NewsApiClient(api_key='b71d9eefeca94a1487e7fc9bf9964af8')
+
+keyword = "stellar"
+
+pagenum = 1
+num_pages = 10
+
+
+for pagenum in range(2, num_pages + 1):
+	
+	data = newsapi.get_everything(q=keyword,
+                                  language='en',
+                                  sources='techcrunch',
+                                  page=pagenum,
+								  sort_by='publishedAt',
+                                  )
+	load = data['articles']
+
+	for post in load:
+		title = post['title']
+		description = post['description'] 
+		if keyword in title :
+			print(' - ' + title)
+			print(description)
+			print(' ')
+		else: 
+			pass
