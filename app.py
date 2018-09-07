f2a50dd619ce2131a513a08852de0e93086aa0c7
Youssef <ynakles@gmail.com>
2018-09-07 08:28:28 +0000

    slight changes

--- app.py
+++ app.py
@@ -139,5 +139,9 @@ def funding():
 def contact():
 	return render_template('contact.html')
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
