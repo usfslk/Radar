4d614f4435118837d7d0203f8ae50a4bcf70c5f4
Youssef Selkani <Ynakles@gmail.com>
2018-09-07 01:03:57 +0000

    v4

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ newsapi = NewsApiClient(api_key='b71d9eefeca94a1487e7fc9bf9964af8')
 
 @app.route('/')
 def main():	
-	conn = sqlite3.connect("homepage.db")
+	conn = sqlite3.connect(":memory:")
+	
 	cursor = conn.cursor()
 	keyword= 'Cryptocurrencies'
 	cursor.execute(""" 
@@ -25,38 +26,39 @@ def main():
 
 	data = newsapi.get_everything(q=keyword,
 	                              language='en',
-	                              sources='techcrunch,crypto-coins-news,the-verge',
-								  sort_by='publishedAt',
+	                              sources='techcrunch,the-verge, bloomberg, financial-times,google-news, fortune, hacker-news',
+								  sort_by='relevancy',
 	                              )
 
 	load = data['articles']
 	resultslist = []
 	scorelist = []
 	index = 0
-	limit = 20
+	limit = 12
 
 	for index, post in zip(range(limit), load):
 		title = post['title']
-		if len(cursor.execute("SELECT Title FROM main WHERE Title = ?", (title,)).fetchall())  > 0:
-			pass
-		else:
-			description = post['description']
-			url = post['url']
-			imglink = post['urlToImage']
-			datetime = post['publishedAt']
-			scoredesc = indicoio.sentiment(title) * 1.5
-			calc = (scoredesc*100)
-			verg = calc / 2
-			score = ("%.0f" % verg)
-			scorelist.append(float(score))
-			nline = title, description, url, imglink, score
-			cursor.execute('INSERT INTO main (Title, Description, URL, IMGLink, Score) VALUES (?, ?, ?, ?, ?)', (nline))
-			conn.commit()
+		description = post['description']
+		url = post['url']
+		imglink = post['urlToImage']
+		datetime = post['publishedAt']
+		scoredesc = indicoio.sentiment(description)
+		calc = (scoredesc*100)
+		score = ("%.2f" % calc)
+		scorelist.append(float(score))
+		nline = title, description, url, imglink, score
+		cursor.execute('INSERT INTO main (Title, Description, URL, IMGLink, Score) VALUES (?, ?, ?, ?, ?)', (nline))
+		conn.commit()
 
 	for row in cursor.execute("SELECT Title, Description, URL, IMGLink, Score, sqltime FROM main ORDER BY Score DESC, sqltime DESC LIMIT 15"):
 		resultslist.append(row)
 
-	return render_template('main.html', resultslist=resultslist, keyword=keyword)
+	sumlist = (sum(scorelist))
+	lenght_list = (len(scorelist))
+	before = (sumlist/lenght_list)
+	average = float("%.0f" % before)
+
+	return render_template('main.html', resultslist=resultslist, keyword=keyword, average=average)
 
 
 ###
@@ -81,44 +83,38 @@ def results():
 
 	resultslist = []
 	scorelist = []
+	index = 0
+	limit = 12
 
-	pagenum = 1
-	num_pages = 10
+	data = newsapi.get_everything(    q=keyword,
+									  sources='crypto-coins-news',
+	                                  language='en',
+	                                  sort_by='publishedAt',
+								 )
+	load = data['articles']
 
-
-	for pagenum in range(2, num_pages + 1):
-		data = newsapi.get_everything(    q=keyword,
-										  sources='crypto-coin-news',
-		                                  language='en',
-		                                  sort_by='relevancy',
-		                                  page=pagenum,
-									 )
-		load = data['articles']
-
-		for post in load:
-			title = post['title']
-			description = post['description']
-			url = post['url']
-			imglink = post['urlToImage']
-			scoredesc = indicoio.sentiment(title)
-			calc = (scoredesc*100)
-			score = ("%.2f" % calc)
-			scorelist.append(float(score))
-			nline = keyword, title, description, url, imglink, score
-			cursor.execute('INSERT INTO main (Keyword, Title, Description, URL, IMGLink, Score)  VALUES (?, ?, ?, ?, ?, ?)', (nline))
-			conn.commit()
+	for index, post in zip(range(limit), load):
+		title = post['title']
+		description = post['description']
+		url = post['url']
+		imglink = post['urlToImage']
+		scoredesc = indicoio.sentiment(title)
+		calc = (scoredesc*100)
+		score = ("%.2f" % calc)
+		scorelist.append(float(score))
+		nline = keyword, title, description, url, imglink, score
+		cursor.execute('INSERT INTO main (Keyword, Title, Description, URL, IMGLink, Score)  VALUES (?, ?, ?, ?, ?, ?)', (nline))
+		conn.commit()
 
 	for row in cursor.execute("SELECT Title, Description, URL, IMGLink, Score, sqltime FROM main ORDER BY Score DESC LIMIT 16"):
 		resultslist.append(row)
 
-	# for row in cursor.execute("SELECT Score FROM main ORDER BY sqltime DESC LIMIT 20"):
-	# 	scorelist.append(row[0]) 
-	# sumlist = (sum(scorelist))
-	# lenght_list = (len(scorelist))
-	# before = (sumlist/lenght_list)
-	# average = float("%.0f" % before)
+	sumlist = (sum(scorelist))
+	lenght_list = (len(scorelist))
+	before = (sumlist/lenght_list)
+	average = float("%.0f" % before)
 
-	return render_template('analysis.html', resultslist=resultslist, keyword=keyword)
+	return render_template('analysis.html', resultslist=resultslist, keyword=keyword, average=average)
 
 
 @app.route('/analytics')
